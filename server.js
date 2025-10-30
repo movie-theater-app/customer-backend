aee757ee1688a93bb48d35e4a3647c480e75b3ea
Anniina-55 <anniina.leikas55@gmail.com>
2025-10-30 17:41:31 +0200

    Updated conf and server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) =>  {
     res.json({message: 'Hello world from NorthStar Movie theatre!'})
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
