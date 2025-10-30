765f3d2ca232a02693bbd6c6ff669a8c5d92b774
Anniina-55 <anniina.leikas55@gmail.com>
2025-10-30 22:54:53 +0200

    Switched message not to be json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) =>  {
-    res.json({message: 'Hello world from NorthStar Movie theatre!'})
+    res.send('Hello world from NorthStar Movie theatre!')
 });
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
