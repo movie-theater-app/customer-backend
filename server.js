af4f0cbce1f999622bfeedd30e3144e745576d46
Lyren <vaino.s.makinen@gmail.com>
2025-11-06 04:31:22 +0200

    Separated theaters from main file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const { query } = require('./db');
 const PORT = process.env.PORT || 3000;
+const theatersRouter = require('./theaters');
 
 app.use(express.json());
 
@@ -9,39 +9,6 @@ app.get('/', (req, res) =>  {
     res.send('Hello world from NorthStar Movie theatre!')
 });
 
-app.post('/theaters/add', async (req, res) => {
-    try {
-        const { name, address, contact_information } = req.body;
-        
-        if (!name || !address || !contact_information) {
-            return res.status(400).json({
-                error: 'All information must be filled'
-            });
-        }
-
-        const result = await query(
-            'INSERT INTO theaters (name, address, contact_information) VALUES ($1, $2, $3) RETURNING *',
-            [name, address, contact_information]
-        );
-
-        res.status(201).json({
-            message: 'Theater added successfully',
-            theater: result.rows[0]
-        });
-    } catch (error) {
-        console.error('Error adding theater:', error);
-        res.status(500).json({ error: 'Failed to add theater' });
-    }
-});
-
-app.get('/theaters', async (req, res) => {
-    try {
-        const result = await query('SELECT * FROM theaters');
-        res.status(200).json(result.rows);
-    } catch (error) {
-        console.error('Error fetching theaters:', error);
-        res.status(500).json({ error: 'Failed to fetch theaters' });
-    }
-});
+app.use('/theaters', theatersRouter);
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
