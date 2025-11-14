698ce5bd9745f42fa2b759475b665d6c52dc3778
Anniina-55 <anniina.leikas55@gmail.com>
2025-11-14 03:48:25 +0200

    Added seats-router and installed swagger

--- server.js
+++ server.js
@@ -6,21 +6,23 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
 
 const PORT = process.env.PORT || 3001;
-const theatersRouter = require('./theaters');
-//const auditoriumRoutes = require('./src/routes/auditoriumRoutes');
-//const reservationRoutes = require('./src/routes/reservationRoutes');
+
+const theatersRouter = require('./src/routes/theaterRoutes');
+const auditoriumsRouter = require('./src/routes/auditoriumRoutes');
+const seatRoutes = require('./src/routes/seatRoutes');
 
 app.use(cors());
 app.use(express.json());
-// see documentation at http://localhost:3001/api-docs
+// Swagger UI -> see documentation at http://localhost:3001/api-docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (req, res) =>  {
     res.send('Hello world from NorthStar Movie theatre!')
 });
 
-app.use('/theaters', theatersRouter);
-//app.use('/auditoriums', auditoriumRoutes);
-//app.use('/reservations', reservationRoutes);
+// Routes
+app.use('/api/theaters', theatersRouter);
+app.use('/api/auditoriums', auditoriumsRouter);
+app.use('/api/seats', seatRoutes);
 
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
