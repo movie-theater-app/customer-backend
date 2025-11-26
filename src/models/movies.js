2a84bb0424d8d28353468098c4b8b6739503205a
Anniina-55 <anniina.leikas55@gmail.com>
2025-11-26 14:23:45 +0200

    Fixed import paths

--- src/models/movies.js
+++ src/models/movies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { query } = require('../src/db/db.js');
+const { query } = require('../db/db.js');
 
 router.get('/', async (req, res) => {
     try {
